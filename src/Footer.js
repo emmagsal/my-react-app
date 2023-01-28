95ae5877fdb1c001e0a20fe2360edccc63693c25
Emma <emmagsalisbury@gmail.com>
2023-01-28 10:51:37 +0000

    added github link

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Footer() {
+    return (
+        <div>
+            <p>
+                <a href="https://github.com/emmagsal/my-react-app">GitHub Repository </a>
+           by Emma Salisbury </p>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import Form from "./Form";
+import Footer from "./Footer";
 
 import "./index.css";
 
@@ -13,6 +14,7 @@ root.render(
   <StrictMode>
     <App />
     <Form />
+    <Footer />
   </StrictMode>
 );
 
