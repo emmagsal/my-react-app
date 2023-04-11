9619894401fc44ed2b3a507252c1f76fd5c1b5a2
Emma <emmagsalisbury@gmail.com>
2023-04-11 18:56:27 +0100

    Improving CSS

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Form.css";
+import "./index.css";
 
 export default function Form() {
   let [loaded, setLoaded] = useState(false);
